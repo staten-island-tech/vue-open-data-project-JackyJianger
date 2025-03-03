79a1d57f2395e2b583f0239653469807359a2aae
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2025-03-03 12:32:50 -0500

    a

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const satData = ref('')
+async function getData() {
+  let res = await fetch(`https://data.cityofnewyork.us/resource/f9bf-2cp4.json`)
+  let data = await res.json()
+  satData.value = data
+  console.log(data)
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
