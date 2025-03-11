285d40fa8216fe974a398f8a13134da39e279b8c
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2025-03-11 12:37:17 -0400

    a

--- app/src/components/FirstChart.vue
+++ app/src/components/FirstChart.vue
@@ -3,30 +3,7 @@
         <canvas id="myChart"></canvas>
     </div>
 </template>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script >
-  const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+<script setup>
+import { ref, onMounted,  watch } from 'vue'
+import Chart from 'chart.js/auto'
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div>
   <FirstChart :satData="satData" />
   </div>
 </template>
